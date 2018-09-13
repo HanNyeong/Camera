05d65155b08decaa6aeadd26751c68ce401529c5
ky <cynicalkim@gmail.com>
2018-09-13 10:21:09 +0900

    포트리스트 분리, 스낵바 추가

--- Connect.py
+++ Connect.py
@@ -1,6 +1,3 @@
-import sys
-import glob
-import serial
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
@@ -8,51 +5,47 @@ from kivymd.theming import ThemeManager
 # from kivymd.navigationdrawer import NavigationDrawer
 from kivymd.bottomsheet import MDListBottomSheet, MDGridBottomSheet
 from kivymd.label import Label
-
-def serial_ports():
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+from kivymd.snackbar import Snackbar
+from kivy.clock import Clock
+from getPortList import getPortLIst
 
 class Connect(App):
     theme_cls = ThemeManager()
     nav_drawer = ObjectProperty()
 
+    returnList = getPortLIst()
     port_list = []
-    for text in serial_ports():
+
+    value = ""
+
+    for text in returnList.serial_ports():
         port_list.append({'viewclass':'MDMenuItem','text':text})
 
     def change_variable(self, value):
+        self.value = value
         self.root.ids.portLabel.text = value
 
     def build(self):
         pass
 
+    def change_screen(self):
+        self.root.ids.scr_mngr.current = 'camera'
+        self.root.ids.toolbar.title = 'Camera'
+        self.root.ids.spinner.active = False
+
     def serialConnect(self):
         self.root.ids.spinner.active = True
+        Snackbar(text="connecting...").show()
         checkConnection = True
         # connection = serialConnect
         if(checkConnection):
-            self.root.ids.scr_mngr.current = 'camera'
-            self.root.ids.toolbar.title = 'Camera'
+            Snackbar(text="connect success").show()
+            Clock.schedule_once(lambda dt: self.change_screen(), 3)
+        else:
+            Snackbar(text="connect fail").show()
             self.root.ids.spinner.active = False
 
-    def show_example_bottom_sheet(self):
+    def bottom_camera_list(self):
         bs = MDListBottomSheet()
         bs.add_item("User LIst", lambda x: x)
         bs.add_item("Face Regist", lambda x: x)
